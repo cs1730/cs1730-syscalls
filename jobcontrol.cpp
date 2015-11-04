1dec69685c5e0021efe263d29c9a399d75ba6ca2
Michael E. Cotterell <mepcotterell@gmail.com>
2015-11-03 19:19:34 -0500

    updated job control example

--- jobcontrol.cpp
+++ jobcontrol.cpp
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <signal.h>
 
+using std::cerr;
 using std::cout;
 using std::endl;
 
@@ -16,16 +17,20 @@ int main() {
   enforce_background();
   struct sigaction sa;
   sa.sa_handler = sig_handler;
-  sigaction(SIGCONT, &sa, nullptr);
-  sigaction(SIGTSTP, &sa, nullptr);
-  //  sigaction( SIGINT, &sa, nullptr);
+  if (sigaction(SIGCONT, &sa, nullptr) == -1) perror("sigaction");
+  if (sigaction(SIGTSTP, &sa, nullptr) == -1) perror("sigaction");
+  if (sigaction( SIGINT, &sa, nullptr) == -1) perror("sigaction");
   while (true) pause();
   return EXIT_SUCCESS;
 } // main
 
 void enforce_background() {
-  if (tcgetpgrp(STDOUT_FILENO) == getpgrp()) {
-    cout << "need to be in background" << endl;
+  int pgid;
+  if ((pgid = tcgetpgrp(STDOUT_FILENO)) == getpgrp()) {
+    cerr << "need to be in background" << endl;
+    exit(EXIT_FAILURE);
+  } else if (pgid == -1) {
+    perror("tcgetpgrp");
     exit(EXIT_FAILURE);
   } // if
 } // enforce_background
@@ -34,23 +39,6 @@ void sig_handler(int signo) {
   cout << "Caught signal " 
        << signo << " (" << strsignal(signo) << ")"
        << endl;
-  switch (signo) {
-  case SIGCONT:
-    cout << "even though we handled this (SIGCONT), the process still continues if stopped."
-	 << endl;
-    break;
-  case SIGTSTP:
-    cout << "since we handled this (SIGTSTP), it will not stop"
-	 << endl;
-    break;
-  case SIGINT:
-    cout << "nice try! can't SIGINT this..."
-	 << endl;
-    break;
-  default:
-    cout << "this should never happen! (we hope)"
-	 << endl;
-  } // switch
 } // sig_handler
 
 
